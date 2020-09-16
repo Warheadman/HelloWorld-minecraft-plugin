3264f030810cb69c28d537554b09c45085229747
Warhead_man <66221215+Warheadman@users.noreply.github.com>
2020-09-16 11:07:36 +0200

    Update HelloCommand.java

--- HelloCommand.java
+++ HelloCommand.java
@@ -1,12 +1,37 @@
-package me.warhead.helloworld;
+package me.warhead.helloworld.commands;
 
-import org.bukkit.plugin.java.JavaPlugin;
-import me.warhead.helloworld.commands.HelloCommand;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
-public class Main extends JavaPlugin {
+import me.warhead.helloworld.Main;
+
+public class HelloCommand implements CommandExecutor {
+	
+	@SuppressWarnings("unused")
+	private Main plugin;
+	
+	public HelloCommand(Main plugin){
+		this.plugin = plugin;
+		plugin.getCommand("hello").setExecutor(this);
+	}
 	
 	@Override
-	public void OnEnable() {
-		new HelloCommand(this);
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		if (!(sender instanceof Player)){
+			sender.sendMessage("Only Player may execute this command");
+			return true;
+		}
+		Player p = (Player) sender;
+		
+		if (p.hasPermission("hello.use")){
+			p.sendMessage("hi!");
+			return true;
+		} else {
+			p.sendMessage("You Dont Have Permissions To Do That!");
+		}
+		return false;
 	}
+	
 }
